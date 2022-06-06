3b562157789d7b317b43b02c955f34f76410e002
Valentine Muchiri <valentine.muchiri@aiesec.net>
2022-06-06 16:05:59 -0700

    4-puts.c3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -16,29 +16,6 @@ int main(void)
 
 {
 
-printf("with proper grammar, but the outcome is a piece of art,\n");
-
-return (0);
-
-}
-#include <stdio.h>
-
-
-
-/**
-
- * main - Entry point
-
- *
-
- * Return: Always 0 (Success)
-
- */
-
-int main(void)
-
-{
-
 puts("\"Programming is like building a multilingual puzzle");
 
 return (0);
