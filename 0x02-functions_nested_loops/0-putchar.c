3159e5dea7a13c8695e925ae730bf75a8231487f
Valentine Muchiri <valentine.muchiri@aiesec.net>
2022-06-08 15:31:49 -0700

    putchar1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+/**
+ * main- prints '_putchar'
+ * Return: Always 0 
+*/
+
+int main(void) 
+{  
+    char word[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+    
+    unsigned int i;
+    
+    for (i = 0; i < sizeof(word); i++)
+    {
+        putchar(word[i]);
+    }
+    -putchar('\n');
+    
+    return 0;
+}
